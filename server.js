3eb01b17593a9ae90aa6d356c9549e34d3e6551a
Kristina Chausheva <k.chausheva@gmail.com>
2020-05-17 11:40:21 +0300

    connecting mongodb with mongoose

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-app.get('/', (req, res) => res.send('API Running'));
+
+connectDB();
+
+app.get('/', (req, res) => res.send('api runnign'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server listen on port ${PORT}`));
