9023334cb2c7f6adf6718792817270b16f35a74e
Kristina Chausheva <k.chausheva@gmail.com>
2020-05-17 22:46:17 +0300

    user registertion

--- config/db.js
+++ config/db.js
@@ -4,8 +4,12 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
-    console.log('mongodb connected');
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log('mongodb connected...');
   } catch (error) {
     console.error(error.message);
     process.exit(1);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @ route get api/users
 // @ desc register user
@@ -15,13 +19,48 @@ router.post(
       'Please enter a password with 6 or more charachters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('User route');
+    const { name, email, password } = req.body;
+
+    try {
+      // see if user exist
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ eroors: [{ msg: 'User already exist' }] });
+      }
+      // get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      // encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.passwod = await bcrypt.hash(password, salt);
+      await user.save();
+
+      //return jwt
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
   }
 );
 
